99273b7752389909d2e11722a8a045d08198a3e5
Shibli Raihan <shibli.raihan.bangli@gmail.com>
2023-10-28 04:26:01 +0600

    Complete this

--- index.php
+++ index.php
@@ -1,27 +1,26 @@
 <?php
 define('APP_ROOT', __dir__);
 
-require_once(APP_ROOT . '/vendor/autoload.php');
+require_once (APP_ROOT. '/vendor/autoload.php');
 
 
 // autoloader for namespaced classes
-spl_autoload_register(function ($class) {
+spl_autoload_register(function($class){
 
-    $classFile = str_replace("\\", DIRECTORY_SEPARATOR, $class . '.php');
+	$classFile = str_replace("\\", DIRECTORY_SEPARATOR, $class.'.php');
 
-    $classPath = APP_ROOT . '/app/' . $classFile;
+	$classPath = APP_ROOT.'/app/'.$classFile;
 
-    if (file_exists($classPath)) {
-        require_once($classPath);
-    }
+	if(file_exists($classPath)){
+		require_once($classPath);
+	}
 });
 
 session_start();
 
 use App\Services\Route;
-
 $route = new Route();
 
-require_once(APP_ROOT . '/routes/route.php');
+require_once(APP_ROOT.'/routes/route.php');
 
 $route->handle();
